a507c171d031f581a72b98743f384a3ce77badc4
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-12 02:09:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song 
-  @@song_count = 0 
+  @@count = 0 
   
-  attr_accessor :artists, :genres, :name
+  attr_accessor :name, :artists, :genres
   
   def initialize
     @@song_count =+ 1 
