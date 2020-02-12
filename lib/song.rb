81b12f7397d677e0980479f4f45c2bce09da0cbe
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-12 02:36:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,7 @@ class Song
     @@genres.each do |genre| 
       if genre_count[genre]
       genre_count[genre] += 1 
-      else
-      genre_count[genre] = 1
+   
       end
     end
     genre_count
